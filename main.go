6fc254943b05496ecfdccaee03c37c62f6c1b400
em <evghenii.maslennikov@gmail.com>
2021-10-23 11:19:45 +0300

    simple calculator

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello calculator")
+
+	fmt.Println("Enter 2 operands")
+	var x, y float64
+	if _, err := fmt.Scanf("%f %f", &x, &y); err != nil {
+		fmt.Printf("invalid operands: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Enter operation")
+	var op string
+	if _, err := fmt.Scan(&op); err != nil {
+		fmt.Printf("invalid operation: %s\n", err)
+		os.Exit(1)
+	}
+
+	var result float64
+	switch op {
+	case "+":
+		result = x + y
+	case "-":
+		result = x - y
+	case "/":
+		if y == 0 {
+			fmt.Println("can not divide by zero")
+			os.Exit(1)
+		}
+		result = x / y
+	case "*":
+		result = x * y
+	}
+
+	fmt.Printf("Result = %.2f\n", result)
 }
